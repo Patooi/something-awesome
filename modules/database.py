0cd824f33a5589c0e57640af6050a8bbe97857d6
Patrick <z5476349@ad.unsw.edu.au>
2024-10-30 18:35:10 +1100

    Added ability to delete users and passwords

--- modules/database.py
+++ modules/database.py
@@ -1,8 +1,19 @@
 import sqlite3
+from contextlib import contextmanager
+
+"""
+Connects to the database and creates the database file, if it does not exist. Context manager ensures that 
+the connection is always closed after process is done and/or exits due to errors.
+"""
 
 
+@contextmanager
 def connect_db():
-    return sqlite3.connect("../db/password_manager.db")
+    conn = sqlite3.connect("../password_manager.db")
+    try:
+        yield conn
+    finally:
+        conn.close()
 
 
 def create_tables(cursor):
@@ -37,7 +48,7 @@ def add_user(user, hashed_master, salt):
                 (user, hashed_master, salt),
             )
     except sqlite3.Error as e:
-        print(f"An error occured while adding the user: {e}")
+        raise RuntimeError(f"An error occured while adding the user: {e}")
 
 
 def add_password(encrypted_password, userID, salt, name):
@@ -51,7 +62,7 @@ def add_password(encrypted_password, userID, salt, name):
                 (userID, encrypted_password, salt, name),
             )
     except sqlite3.Error as e:
-        print(f"An error occured while adding the password: {e}")
+        raise RuntimeError(f"An error occured while adding the password: {e}")
 
 
 def get_userID(user):
@@ -64,10 +75,10 @@ def get_userID(user):
             ).fetchone()
             return userID
     except sqlite3.Error as e:
-        print(f"An error occured while retrieving userID: {e}")
+        raise RuntimeError(f"An error occured while retrieving userID: {e}")
 
 
-def get_PasswordID(userID, name):
+def get_passwordID(userID, name):
     try:
         with connect_db() as conn:
             cursor = conn.cursor()
@@ -81,18 +92,62 @@ def get_PasswordID(userID, name):
             ).fetchone()
             return passID
     except sqlite3.Error as e:
-        print(f"An error occured while retrieving passID: {e}")
+        raise RuntimeError(f"An error occured while retrieving passID: {e}")
 
 
-def get_Password(passwordID):
+def get_password(passwordID):
     try:
         with connect_db() as conn:
             cursor = conn.cursor()
 
             password = cursor.execute(
-                "SELECT password FROM Passwords WHERE passwordID is ?", (passwordID,)
+                "SELECT password, salt FROM Passwords WHERE passwordID is ?",
+                (passwordID,),
             ).fetchone()
 
             return password
     except sqlite3.Error as e:
-        print(f"An error occured while retrieving Password: {e}")
+        raise RuntimeError(f"An error occured while retrieving Password: {e}")
+
+
+def get_all_passwords(userID):
+    try:
+        with connect_db() as conn:
+            cursor = conn.cursor()
+
+            passwords = cursor.execute(
+                "SELECT password, salt FROM Passwords WHERE userID is ?", (userID,)
+            ).fetchall()
+
+            return passwords
+    except sqlite3.Error as e:
+        raise RuntimeError(f"An error occured while retrieving Passwords: {e}")
+
+
+def delete_password(passwordID):
+    try:
+        with connect_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Passwords WHERE passwordID is ?", (passwordID,))
+    except sqlite3.Error as e:
+        raise RuntimeError(f"An error occured while deleting the password: {e}")
+
+
+def delete_all_passwords(userID):
+    try:
+        with connect_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Passwords WHERE userID is ?", (userID,))
+    except sqlite3.Error as e:
+        raise RuntimeError(f"An error occured while deleting the user's passwords: {e}")
+
+
+def delete_user(userID):
+    try:
+        with connect_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM Users WHERE userID is ?", (userID,))
+
+            delete_all_passwords(userID)
+    except sqlite3.Error as e:
+        raise RuntimeError(f"An error occured while deleting the user: {e}")
